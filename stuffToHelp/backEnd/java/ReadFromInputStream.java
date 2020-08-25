b69f624c8bed35aa838f72696a41615b822d4a3b
waeljawadi <42736820+waeljawadi@users.noreply.github.com>
2020-08-25 15:54:50 +0100

    Create ReadFromInputStream.java

---
+++ stuffToHelp/backEnd/java/ReadFromInputStream.java
@@ -0,0 +1,10 @@
+private String readFromInputStream(InputStream inputStream) throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
+    }
